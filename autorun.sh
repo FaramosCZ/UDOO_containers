735944d8307929acc3aa30c96cdc7dc68bd34e57
LOD <lod@udoo.server>
2020-03-31 17:21:12 +0200

    Fix loading of the client data Update client data (accept license, and so on)

--- autorun.sh
+++ autorun.sh
@@ -1,5 +1,7 @@
 # First we need to create a Host Pulseaudio unix socket so we can mount it into the container
+echo -e "\nCreating PA socket ..."
 pactl load-module module-native-protocol-unix auth-anonymous=1 socket=/tmp/pulseaudio.socket
+echo -e "Done\n"
 
 
 
@@ -17,13 +19,12 @@ EOF
 
 # Now we can finally run the container
 podman run \
-  -ti --network host \
+  -d --network host \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   --env PULSE_SERVER=unix:/tmp/pulseaudio.socket \
   --env PULSE_COOKIE=/tmp/pulseaudio.cookie \
   --volume /tmp/pulseaudio.socket:/tmp/pulseaudio.socket \
   --volume /tmp/pulseaudio.client.conf:/etc/pulse/client.conf \
   -e DISPLAY=:0 \
+  -v /home/lod/UDOO_containers/data/settings.db:/home/user/.ts3client/settings.db:rw \
   ts3_client
-
-#  -v /home/lod/UDOO_containers/data/settings.db:/home/user/TeamSpeak3-Client-linux_amd64/config/settings.db \

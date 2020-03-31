86cb1d62c7bf0218864c6b4ac66426dfd594f059
LOD <lod@udoo.server>
2020-03-31 18:17:29 +0200

    Fix the scripts so they can be run from random paths Fix datafiles permissions before each use

--- ts3_client/autorun.sh
+++ ts3_client/autorun.sh
@@ -1,3 +1,21 @@
+#!/bin/bash
+
+#----------------------------------------
+# SET UP A RELATIVE PATH
+
+# Determine and set the relative path we are on
+#   $BASH_SOURCE is the only trustworthy source of information. But it does not follow symlinks - use readlink to get>
+relative_path=$( readlink -f "$BASH_SOURCE" )
+# Move to the current script location, to assure relative paths will be configured correctly
+pushd "${relative_path%/*}" || exit
+
+#----------------------------------------
+# RUN ALL THE AUTORUN SCRIPTS
+
+set -vx
+
+
+
 # First we need to create a Host Pulseaudio unix socket so we can mount it into the container
 echo -e "\nCreating PA socket ..."
 pactl load-module module-native-protocol-unix auth-anonymous=1 socket=/tmp/pulseaudio.socket
@@ -17,14 +35,26 @@ EOF
 
 
 
+# Make the data files readable even for non-privileged container user
+chmod a+rw data/*
+
+
+
 # Now we can finally run the container
 podman run \
   -d --network host \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
-  --env PULSE_SERVER=unix:/tmp/pulseaudio.socket \
-  --env PULSE_COOKIE=/tmp/pulseaudio.cookie \
-  --volume /tmp/pulseaudio.socket:/tmp/pulseaudio.socket \
-  --volume /tmp/pulseaudio.client.conf:/etc/pulse/client.conf \
+  -v /tmp/pulseaudio.socket:/tmp/pulseaudio.socket \
+  -v /tmp/pulseaudio.client.conf:/etc/pulse/client.conf \
+  -e PULSE_SERVER=unix:/tmp/pulseaudio.socket \
+  -e PULSE_COOKIE=/tmp/pulseaudio.cookie \
   -e DISPLAY=:0 \
-  -v /home/lod/UDOO_containers/data/settings.db:/home/user/.ts3client/settings.db:rw \
+  -v `pwd`/data/settings.db:/home/user/.ts3client/settings.db \
   ts3_client
+
+
+
+#----------------------------------------
+
+# Jump back to the directory we were before this script execution
+popd
--- ts3_server/autobuild.sh
+++ ts3_server/autobuild.sh
@@ -1,3 +1,24 @@
+#!/bin/bash
+
+#----------------------------------------
+# SET UP A RELATIVE PATH
+
+# Determine and set the relative path we are on
+#   $BASH_SOURCE is the only trustworthy source of information. But it does not follow symlinks - use readlink to get>
+relative_path=$( readlink -f "$BASH_SOURCE" )
+# Move to the current script location, to assure relative paths will be configured correctly
+pushd "${relative_path%/*}" || exit
+
+#----------------------------------------
+# RUN ALL THE AUTORUN SCRIPTS
+
+set -vx
+
 podman build \
   -f containerfile \
   -t ts3_server
+
+#----------------------------------------
+
+# Jump back to the directory we were before this script execution
+popd
